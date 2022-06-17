6ce48c86c84b9bc05ba666108426d40ba5fdc995
magisystem0408 <masato.matsudo@gmail.com>
2022-06-17 22:03:58 +0900

    context

--- main.go
+++ main.go
@@ -1,11 +1,39 @@
 package main
 
-//大文字はパブリック
-var Public string = "Public"
+import (
+	"context"
+	"fmt"
+	"time"
+)
 
-//小文字はプライベート
-var private string = "Private"
-
+func longProcess(ctx context.Context, ch chan string) {
+	fmt.Println("run")
+	time.Sleep(2 * time.Second)
+	fmt.Println("finish")
+	ch <- "result"
+}
 func main() {
+	ch := make(chan string)
+	//タイムアウトの設定などに使える
+	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(ctx, 3*time.Second)
 
+	//空のcontextを使用したい時、TODO()
+	//ctx := context.TODO()
+
+	defer cancel()
+	go longProcess(ctx, ch)
+
+CTXLOOP:
+	for {
+		select {
+		case <-ctx.Done():
+			fmt.Println(ctx.Err())
+			break CTXLOOP
+		case <-ch:
+			fmt.Println("success")
+			break CTXLOOP
+		}
+	}
+	fmt.Println("###########")
 }
